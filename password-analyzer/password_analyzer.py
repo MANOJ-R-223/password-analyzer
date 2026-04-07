9aec2fca82422a8db298c7aa82059ff35bf43c76
MANOJ <deadlyduo223@gmail.com>
2026-04-07 20:28:33 +0530

    Add password strength scoring

--- password-analyzer/password_analyzer.py
+++ password-analyzer/password_analyzer.py
@@ -1,6 +1,32 @@
+import re
 def check_password(password):
-    if len(password)<6:
+    score = 0
+
+    # Length check
+    if len(password) >= 8:
+        score += 1
+
+    # Uppercase
+    if re.search(r"[A-Z]", password):
+        score += 1
+
+    # Lowercase
+    if re.search(r"[a-z]", password):
+        score += 1
+
+    # Numbers
+    if re.search(r"[0-9]", password):
+        score += 1
+
+    # Special characters
+    if re.search(r"[!@#$%^&*]", password):
+        score += 1
+
+    # Strength result
+    if score <= 2:
         return "Weak"
+    elif score == 3 or score == 4:
+        return "Medium"
     else:
         return "Strong"
     
