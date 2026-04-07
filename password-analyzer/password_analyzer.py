4ab67d671e0c40062263d5ec08bda6c9b577950f
MANOJ <deadlyduo223@gmail.com>
2026-04-07 20:35:34 +0530

    Refactored code into modular structure using utils.py

--- password-analyzer/password_analyzer.py
+++ password-analyzer/password_analyzer.py
@@ -1,33 +1,4 @@
-import re
-def check_password(password):
-    score = 0
+from utils import check_password
 
-    # Length check
-    if len(password) >= 8:
-        score += 1
-
-    # Uppercase
-    if re.search(r"[A-Z]", password):
-        score += 1
-
-    # Lowercase
-    if re.search(r"[a-z]", password):
-        score += 1
-
-    # Numbers
-    if re.search(r"[0-9]", password):
-        score += 1
-
-    # Special characters
-    if re.search(r"[!@#$%^&*]", password):
-        score += 1
-
-    # Strength result
-    if score <= 2:
-        return "Weak"
-    elif score == 3 or score == 4:
-        return "Medium"
-    else:
-        return "Strong"
-    
-print(check_password("password"))
+pwd = input("Enter password: ")
+print("Strength:", check_password(pwd))
