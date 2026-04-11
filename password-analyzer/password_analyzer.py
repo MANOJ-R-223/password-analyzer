16922753cc38e34e14c3bf47fca12fdd2276a57e
MANOJ <deadlyduo223@gmail.com>
2026-04-11 11:36:35 +0530

    add CLI menu interface for interactive password analysis

--- password-analyzer/password_analyzer.py
+++ password-analyzer/password_analyzer.py
@@ -7,21 +7,38 @@ def main():
     if not common_passwords:
         print("Dataset not loaded. Skipping leak check.")
 
-    pwd = input("\nEnter password: ")
-    print("\nSHA-256 Hash:", hash_password(pwd))
+    while True:
+        print("\n====== PASSWORD ANALYZER ======")
+        print("1.Enter Password")
+        print("2.Exit")
 
-    if common_passwords and is_common_password(pwd, common_passwords):
-        print("\nWeak Password : Your password is found in leaked dataset")
-    else:
-        strength, suggestions = check_password(pwd)
-        print("\nStrength :",strength)
+        choice = input("\nEnter choice : ")
 
-        if suggestions:
-            print("\nSuggestions to improve:")
-            for s in suggestions:
-                print("\n->",s)
+        if choice == "1":
+
+            pwd = input("\nEnter password: ")
+            print("\nSHA-256 Hash:", hash_password(pwd))
+
+            if common_passwords and is_common_password(pwd, common_passwords):
+                print("\nWeak Password : Your password is found in leaked dataset")
+            else:
+                strength, suggestions = check_password(pwd)
+                print("\nStrength :",strength)
+
+                if suggestions:
+                    print("\nSuggestions to improve:")
+                    for s in suggestions:
+                        print("\n->",s)
     
-    print("\nEstimated crack time: ", estimate_crack_time(pwd))
+            print("\nEstimated crack time: ", estimate_crack_time(pwd))
+
+        elif choice == "2":
+            print("\nExiting....")
+            break
+
+        else:
+            print("Invalid Option. Try Again")
+            
 
 
 if __name__ == "__main__":
